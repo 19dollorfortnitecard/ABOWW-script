fcab6d6e942b2b6c100157c4c27315cd3547f3d6
skelton <103327260+19dollorfortnitecard@users.noreply.github.com>
2022-04-21 22:00:10 -0400

    what
    cool

---
+++ scripts/ABBOW.lua
@@ -0,0 +1,187 @@
+--why the fuck my sister need so much
+local ScreenGui = Instance.new("ScreenGui")
+local MainFrame = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local NameLabel = Instance.new("TextLabel")
+local TextLabel = Instance.new("TextLabel")
+local DarkHub = Instance.new("TextButton")
+local UICorner_2 = Instance.new("UICorner")
+local FunkyFriday = Instance.new("TextButton")
+local UICorner_3 = Instance.new("UICorner")
+local InfinteYield = Instance.new("TextButton")
+local UICorner_4 = Instance.new("UICorner")
+local Killall = Instance.new("TextButton")
+local UICorner_5 = Instance.new("UICorner")
+local MuderMystery2script = Instance.new("TextButton")
+local UICorner_6 = Instance.new("UICorner")
+local TowerOfHellScript = Instance.new("TextButton")
+local UICorner_7 = Instance.new("UICorner")
+local Brookhaven = Instance.new("TextButton")
+local UICorner_8 = Instance.new("UICorner")
+
+
+
+ScreenGui.Parent = game.CoreGui
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+MainFrame.Name = "MainFrame"
+MainFrame.Parent = ScreenGui
+MainFrame.BackgroundColor3 = Color3.fromRGB(170, 170, 255)
+MainFrame.BorderSizePixel = 0
+MainFrame.Position = UDim2.new(0.246025741, 0, 0.161090448, 0)
+MainFrame.Size = UDim2.new(0, 433, 0, 365)
+MainFrame.Active = true
+MainFrame.Draggable = true
+
+UICorner.Parent = MainFrame
+
+NameLabel.Name = "NameLabel"
+NameLabel.Parent = MainFrame
+NameLabel.BackgroundColor3 = Color3.fromRGB(170, 170, 255)
+NameLabel.BackgroundTransparency = 1.000
+NameLabel.Size = UDim2.new(0, 433, 0, 50)
+NameLabel.Font = Enum.Font.PermanentMarker
+NameLabel.Text = "ABOWW SCRIPT "
+NameLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+NameLabel.TextScaled = true
+NameLabel.TextSize = 14.000
+NameLabel.TextWrapped = true
+
+TextLabel.Parent = MainFrame
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.Position = UDim2.new(0, 0, 0.632876694, 0)
+TextLabel.Size = UDim2.new(0, 433, 0, 134)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = "More Coming Soon Baddy Raddies💅"
+TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14.000
+TextLabel.TextWrapped = true
+
+DarkHub.Name = "DarkHub"
+DarkHub.Parent = MainFrame
+DarkHub.BackgroundColor3 = Color3.fromRGB(170, 85, 255)
+DarkHub.BorderSizePixel = 0
+DarkHub.Position = UDim2.new(0.423164308, 0, 0.32713753, 0)
+DarkHub.Size = UDim2.new(0, 166, 0, 31)
+DarkHub.Font = Enum.Font.SourceSans
+DarkHub.Text = "DarkHub"
+DarkHub.TextColor3 = Color3.fromRGB(0, 0, 0)
+DarkHub.TextScaled = true
+DarkHub.TextSize = 14.000
+DarkHub.TextWrapped = true
+DarkHub.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/RandomAdamYT/DarkHub/master/Init'), true))()
+end)
+
+UICorner_2.Parent = DarkHub
+
+FunkyFriday.Name = "Funky Friday"
+FunkyFriday.Parent = MainFrame
+FunkyFriday.BackgroundColor3 = Color3.fromRGB(170, 85, 255)
+FunkyFriday.BorderSizePixel = 0
+FunkyFriday.Position = UDim2.new(0.258137792, 0, 0.272614598, 0)
+FunkyFriday.Size = UDim2.new(0, 166, 0, 31)
+FunkyFriday.Font = Enum.Font.SourceSans
+FunkyFriday.Text = "Funky Friday"
+FunkyFriday.TextColor3 = Color3.fromRGB(0, 0, 0)
+FunkyFriday.TextScaled = true
+FunkyFriday.TextSize = 14.000
+FunkyFriday.TextWrapped = true
+FunkyFriday.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/wally-rblx/funky-friday-autoplay/main/main.lua",true))()
+end)
+
+
+UICorner_3.Parent = FunkyFriday
+
+InfinteYield.Name = "Infinte Yield"
+InfinteYield.Parent = MainFrame
+InfinteYield.BackgroundColor3 = Color3.fromRGB(170, 85, 255)
+InfinteYield.BorderSizePixel = 0
+InfinteYield.Position = UDim2.new(0.258137792, 0, 0.223048314, 0)
+InfinteYield.Size = UDim2.new(0, 166, 0, 31)
+InfinteYield.Font = Enum.Font.SourceSans
+InfinteYield.Text = "Infinte Yield"
+InfinteYield.TextColor3 = Color3.fromRGB(0, 0, 0)
+InfinteYield.TextScaled = true
+InfinteYield.TextSize = 14.000
+InfinteYield.TextWrapped = true
+InfinteYield.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))() ()
+end)
+
+UICorner_4.Parent = InfinteYield
+
+Killall.Name = "Kill all"
+Killall.Parent = MainFrame
+Killall.BackgroundColor3 = Color3.fromRGB(170, 85, 255)
+Killall.BorderSizePixel = 0
+Killall.Position = UDim2.new(0.423164308, 0, 0.272614598, 0)
+Killall.Size = UDim2.new(0, 166, 0, 31)
+Killall.Font = Enum.Font.SourceSans
+Killall.Text = "Kill All"
+Killall.TextColor3 = Color3.fromRGB(0, 0, 0)
+Killall.TextScaled = true
+Killall.TextSize = 14.000
+Killall.TextWrapped = true
+Killall.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet('https://pastebin.com/raw/eTUAUm2f',true))()
+end)
+
+UICorner_5.Parent = Killall
+
+MuderMystery2script.Name = "Muder Mystery 2 script"
+MuderMystery2script.Parent = MainFrame
+MuderMystery2script.BackgroundColor3 = Color3.fromRGB(170, 85, 255)
+MuderMystery2script.BorderSizePixel = 0
+MuderMystery2script.Position = UDim2.new(0.346707076, 0, 0.382899612, 0)
+MuderMystery2script.Size = UDim2.new(0, 166, 0, 31)
+MuderMystery2script.Font = Enum.Font.SourceSans
+MuderMystery2script.Text = "MM2 Script"
+MuderMystery2script.TextColor3 = Color3.fromRGB(0, 0, 0)
+MuderMystery2script.TextScaled = true
+MuderMystery2script.TextSize = 14.000
+MuderMystery2script.TextWrapped = true
+MuderMystery2script.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/Ethanoj1/EclipseMM2/master/Script"),true))()
+end)
+
+UICorner_6.Parent = MuderMystery2script
+
+TowerOfHellScript.Name = "Tower Of Hell Script"
+TowerOfHellScript.Parent = MainFrame
+TowerOfHellScript.BackgroundColor3 = Color3.fromRGB(170, 85, 255)
+TowerOfHellScript.BorderSizePixel = 0
+TowerOfHellScript.Position = UDim2.new(0.257380813, 0, 0.32713753, 0)
+TowerOfHellScript.Size = UDim2.new(0, 166, 0, 31)
+TowerOfHellScript.Font = Enum.Font.SourceSans
+TowerOfHellScript.Text = "TOH script"
+TowerOfHellScript.TextColor3 = Color3.fromRGB(0, 0, 0)
+TowerOfHellScript.TextScaled = true
+TowerOfHellScript.TextSize = 14.000
+TowerOfHellScript.TextWrapped = true
+TowerOfHellScript.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/2dgeneralspam1/scripts-and-stuff/master/scripts/garfield%20hub", true))()
+end)
+
+UICorner_7.Parent = TowerOfHellScript
+
+Brookhaven.Name = "Brookhaven"
+Brookhaven.Parent = MainFrame
+Brookhaven.BackgroundColor3 = Color3.fromRGB(170, 85, 255)
+Brookhaven.BorderSizePixel = 0
+Brookhaven.Position = UDim2.new(0.423921287, 0, 0.223048314, 0)
+Brookhaven.Size = UDim2.new(0, 166, 0, 31)
+Brookhaven.Font = Enum.Font.SourceSans
+Brookhaven.Text = "Brookhaven"
+Brookhaven.TextColor3 = Color3.fromRGB(0, 0, 0)
+Brookhaven.TextScaled = true
+Brookhaven.TextSize = 14.000
+Brookhaven.TextWrapped = true
+Brookhaven.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/IceMael7/NewIceHub/main/Brookhaven",true))()
+end)
+
+UICorner_8.Parent = Brookhaven
